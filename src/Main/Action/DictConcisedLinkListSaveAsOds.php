539136b96efd2cc6841e0fb008bb3d64f4823e24
samwhelp <samwhelp@users.noreply.github.com>
2019-10-04 23:02:24 +0800

    add message

--- src/Main/Action/DictConcisedLinkListSaveAsOds.php
+++ src/Main/Action/DictConcisedLinkListSaveAsOds.php
@@ -11,12 +11,17 @@ class DictConcisedLinkListSaveAsOds {
 			->run()
 		;
 
+
 		// ## 將結果儲存成 ods
+		$file_path = THE_CLI_VAR_DIR_PATH . '/result/link-result.ods';
+
 		$writer = (new \Dict\LinkResult\SaveAsOds)
-			->setTargetFilePath(THE_CLI_VAR_DIR_PATH . '/result/link-result.ods')
+			->setTargetFilePath($file_path)
 			->setData($model->getTargetData())
 			->run()
 		;
+
+		echo('儲存檔案: ' . $file_path . PHP_EOL);
 
 	}
 
